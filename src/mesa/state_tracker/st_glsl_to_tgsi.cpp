7d40db8cdb62d9f510a1f38e9df5b9552c7e6b20
Dave Airlie <airlied@redhat.com>
2016-06-07 09:39:55 +1000

    st/glsl_to_tgsi: don't increase immediate index by 1.
    Immediates are stored into a separate table, and are
    consolidated, so if we get an immediate we don't need
    to offset it as the index it has is correct.
    
    Cc: "11.2 12.0" <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit 27d456cc87a01998c6fe1dbf45937e2ca6128495)

--- src/mesa/state_tracker/st_glsl_to_tgsi.cpp
+++ src/mesa/state_tracker/st_glsl_to_tgsi.cpp
@@ -818,7 +818,7 @@ glsl_to_tgsi_visitor::emit_asm(ir_instruction *ir, unsigned op,
 
             if (dinst->src[j].type == GLSL_TYPE_DOUBLE) {
                dinst->src[j].index = initial_src_idx[j];
-               if (swz > 1) {
+               if (swz > 1 && dinst->src[j].file != PROGRAM_IMMEDIATE) {
                   dinst->src[j].double_reg2 = true;
                   dinst->src[j].index++;
 	       }
