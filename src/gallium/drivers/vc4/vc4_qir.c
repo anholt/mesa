0ad8046826825cfb435c65b5bae59c909af1a2bb
Eric Anholt <eric@anholt.net>
2016-11-15 12:36:20 -0800

    vc4: Replace the qinst src[] with a fixed-size array.
    This may have made a tiny bit of sense when we had one 4-arg inst per
    shader, but if we only ever put 2 things in, having a pointer to 2 things
    almost every instruction is pointless indirection.
    
    (cherry picked from commit 51087327f2ba929739719b2ae243d8c69d31346f)

--- src/gallium/drivers/vc4/vc4_qir.c
+++ src/gallium/drivers/vc4/vc4_qir.c
@@ -478,7 +478,6 @@ qir_inst(enum qop op, struct qreg dst, struct qreg src0, struct qreg src1)
 
         inst->op = op;
         inst->dst = dst;
-        inst->src = calloc(2, sizeof(inst->src[0]));
         inst->src[0] = src0;
         inst->src[1] = src1;
         inst->cond = QPU_COND_ALWAYS;
@@ -599,7 +598,6 @@ qir_remove_instruction(struct vc4_compile *c, struct qinst *qinst)
                 c->defs[qinst->dst.index] = NULL;
 
         list_del(&qinst->link);
-        free(qinst->src);
         free(qinst);
 }
 
