4902fd8ff434f6b5a4e6dbbde512dbcce02f717b
Jason Ekstrand <jason.ekstrand@intel.com>
2018-03-20 11:11:25 -0700

    nir: Add a deref path helper struct
    This commit introduces a new nir_deref.h header for helpers that are
    less common and really only needed by a few heavy-duty passes.  In this
    header is a new struct for representing a full deref path which can be
    walked in either direction.

--- src/compiler/nir/nir_deref.c
+++ src/compiler/nir/nir_deref.c
@@ -23,6 +23,55 @@
 
 #include "nir.h"
 #include "nir_builder.h"
+#include "nir_deref.h"
+
+void
+nir_deref_path_init(struct nir_deref_path *path,
+                    nir_deref_instr *deref, void *mem_ctx)
+{
+   /* The length of the short path is at most ARRAY_SIZE - 1 because we need
+    * room for the NULL terminator.
+    */
+   static const int max_short_path_len = ARRAY_SIZE(path->_short_path) - 1;
+
+   int count = 0;
+
+   nir_deref_instr **tail = &path->_short_path[max_short_path_len];
+   nir_deref_instr **head = tail;
+
+   *tail = NULL;
+   for (nir_deref_instr *d = deref; d; d = nir_deref_instr_parent(d)) {
+      count++;
+      if (count <= max_short_path_len)
+         *(--head) = d;
+   }
+
+   if (count <= max_short_path_len) {
+      /* If we're under max_short_path_len, just use the short path. */
+      path->path = head;
+      goto done;
+   }
+
+   path->path = ralloc_array(mem_ctx, nir_deref_instr *, count + 1);
+   head = tail = path->path + count;
+   *tail = NULL;
+   for (nir_deref_instr *d = deref; d; d = nir_deref_instr_parent(d))
+      *(--head) = d;
+
+done:
+   assert(head == path->path);
+   assert(tail == head + count);
+   assert((*head)->deref_type == nir_deref_type_var);
+   assert(*tail == NULL);
+}
+
+void
+nir_deref_path_finish(struct nir_deref_path *path)
+{
+   if (path->path < &path->_short_path[0] ||
+       path->path > &path->_short_path[ARRAY_SIZE(path->_short_path) - 1])
+      ralloc_free(path->path);
+}
 
 nir_deref_var *
 nir_deref_instr_to_deref(nir_deref_instr *instr, void *mem_ctx)
