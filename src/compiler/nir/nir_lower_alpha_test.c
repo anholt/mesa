079ae84f8b0c3aa13acc1936932e8028666371be
Jason Ekstrand <jason.ekstrand@intel.com>
2018-03-26 17:06:35 -0700

    nir: Support deref instructions in lower_alpha_test

--- src/compiler/nir/nir_lower_alpha_test.c
+++ src/compiler/nir/nir_lower_alpha_test.c
@@ -58,6 +58,9 @@ nir_lower_alpha_test(nir_shader *shader, enum compare_func func,
                case nir_intrinsic_store_var:
                   out = intr->variables[0]->var;
                   break;
+               case nir_intrinsic_store_deref:
+                  out = nir_deref_instr_get_variable(nir_src_as_deref(intr->src[0]));
+                  break;
                case nir_intrinsic_store_output:
                   /* already had i/o lowered.. lookup the matching output var: */
                   nir_foreach_variable(var, &shader->outputs) {
@@ -85,6 +88,9 @@ nir_lower_alpha_test(nir_shader *shader, enum compare_func func,
                nir_ssa_def *alpha;
                if (alpha_to_one) {
                   alpha = nir_imm_float(&b, 1.0);
+               } else if (intr->intrinsic == nir_intrinsic_store_deref) {
+                  alpha = nir_channel(&b, nir_ssa_for_src(&b, intr->src[1], 4),
+                                      3);
                } else {
                   alpha = nir_channel(&b, nir_ssa_for_src(&b, intr->src[0], 4),
                                       3);
