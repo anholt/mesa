c6aaafa3a1745c0c5a3b4cc0e023b74e2eccbaf4
Christian Gmeiner <christian.gmeiner@gmail.com>
2018-09-01 21:15:27 +0200

    nir: add lowering for ffloor
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -2083,6 +2083,9 @@ typedef struct nir_shader_compiler_options {
     */
    bool fdot_replicates;
 
+   /** lowers ffloor to fsub+ffract: */
+   bool lower_ffloor;
+
    /** lowers ffract to fsub+ffloor: */
    bool lower_ffract;
 
