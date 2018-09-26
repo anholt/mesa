7d77fe1bcc8e84a30ddc5d4bc6b0c571dfd64b22
Eric Anholt <eric@anholt.net>
2018-09-26 09:13:13 -0700

    nir: Expose nir_remove_unused_io_vars().
    For gallium drivers where you want to do some linking at variant compile
    time, you don't have the other producer/consumer shader on hand to modify.
    By exposing the inner function, the driver can have the used varyings in
    the compiled shader cache key and still do linking.
    
    This is also useful for V3D, where the binning shader wants to only output
    position and TF varyings.  We've been removing those after nir_lower_io,
    but this will be less driver-specific code and let more of the shader get
    DCEed early in NIR.
    
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -2778,6 +2778,9 @@ void nir_assign_var_locations(struct exec_list *var_list, unsigned *size,
 
 /* Some helpers to do very simple linking */
 bool nir_remove_unused_varyings(nir_shader *producer, nir_shader *consumer);
+bool nir_remove_unused_io_vars(nir_shader *shader, struct exec_list *var_list,
+                               uint64_t *used_by_other_stage,
+                               uint64_t *used_by_other_stage_patches);
 void nir_compact_varyings(nir_shader *producer, nir_shader *consumer,
                           bool default_to_smooth_interp);
 
