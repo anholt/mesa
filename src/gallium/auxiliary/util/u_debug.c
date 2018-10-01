f1f104e54878ef42f02c947b8f1f3f0b710af6d8
Dylan Baker <dylan@pnwbakers.com>
2018-10-01 11:45:25 -0700

    gallium/util: remove u_inlines.h from u_debug.c
    It's not used, and I'm not pulling u_inlines into src/util.
    
    Tested-by: Brian Paul <brianp@vmware.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>

--- src/gallium/auxiliary/util/u_debug.c
+++ src/gallium/auxiliary/util/u_debug.c
@@ -33,7 +33,6 @@
 #include "util/u_debug.h"
 #include "pipe/p_format.h"
 #include "pipe/p_state.h"
-#include "util/u_inlines.h"
 #include "util/u_string.h"
 #include "util/u_math.h"
 #include <inttypes.h>
