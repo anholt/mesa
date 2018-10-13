cbf9fe50b5f848c6be380ab37cf034618c9a851e
Rob Clark <robdclark@gmail.com>
2018-10-13 12:34:09 -0400

    freedreno: fix off-by-one error in BEGIN_RING()
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- src/gallium/drivers/freedreno/freedreno_util.h
+++ src/gallium/drivers/freedreno/freedreno_util.h
@@ -274,7 +274,7 @@ OUT_RB(struct fd_ringbuffer *ring, struct fd_ringbuffer *target)
 
 static inline void BEGIN_RING(struct fd_ringbuffer *ring, uint32_t ndwords)
 {
-	if (ring->cur + ndwords >= ring->end)
+	if (ring->cur + ndwords > ring->end)
 		fd_ringbuffer_grow(ring, ndwords);
 }
 
