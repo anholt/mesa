anholt/mesa

commit 6cc79e4411f750471bb35faf8578069a6a2f44d1

radeonsi: fix incorrect hw screen offset and guardband computation
It resulted in assertion failures or incorrect rendering.

Broken by: 9e182b8313c5ab952498a76495f57e8420f9e5ad

src/gallium/drivers/radeonsi/si_state_viewport.c
@@ -176,8 +176,8 @@ static void si_emit_guardband(struct si_context *ctx)
 	/* Determine the optimal hardware screen offset to center the viewport
 	 * within the viewport range in order to maximize the guardband size.
 	 */
-	int hw_screen_offset_x = (vp_as_scissor.maxx - vp_as_scissor.minx) / 2;
-	int hw_screen_offset_y = (vp_as_scissor.maxy - vp_as_scissor.miny) / 2;
+	int hw_screen_offset_x = (vp_as_scissor.maxx + vp_as_scissor.minx) / 2;
+	int hw_screen_offset_y = (vp_as_scissor.maxy + vp_as_scissor.miny) / 2;
 
 	const unsigned hw_screen_offset_max = 8176;
 	/* SI-CI need to align the offset to an ubertile consisting of all SEs. */

commit 2a26b1c0451c9eca16febf76a0881737325fda69

radeonsi: fix gnome-shell crash
I wasn't expecting to get viewports with the center having
negative coordinates.

Broken by: 6cc79e4411f

src/gallium/drivers/radeonsi/si_state_viewport.c
@@ -184,8 +184,8 @@ static void si_emit_guardband(struct si_context *ctx)
 	const unsigned hw_screen_offset_alignment =
 		ctx->chip_class >= VI ? 16 : MAX2(ctx->screen->se_tile_repeat, 16);
 
-	hw_screen_offset_x = MIN2(hw_screen_offset_x, hw_screen_offset_max);
-	hw_screen_offset_y = MIN2(hw_screen_offset_y, hw_screen_offset_max);
+	hw_screen_offset_x = CLAMP(hw_screen_offset_x, 0, hw_screen_offset_max);
+	hw_screen_offset_y = CLAMP(hw_screen_offset_y, 0, hw_screen_offset_max);
 
 	/* Align the screen offset by dropping the low 4 bits. */
 	hw_screen_offset_x &= ~(hw_screen_offset_alignment - 1);