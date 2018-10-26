ValveSoftware/steamos_mesa

commit 83ea8dd99bb16e5d9bb880e64cd2047abc536b70

util: add disk_cache_get_function_identifier()
This can be used as a drop in replacement for
disk_cache_get_function_timestamp().

Here we use build-id to generate a driver-id rather than build
timestamp if available. This should resolve issues such as
distros using reproducable builds and flatpak not having
real build timestamps.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/util/disk_cache.h
@@ -27,6 +27,7 @@
 #ifdef HAVE_DLFCN_H
 #include <dlfcn.h>
 #include <stdio.h>
+#include "util/build_id.h"
 #endif
 #include <assert.h>
 #include <stdint.h>
@@ -111,6 +112,21 @@ disk_cache_get_function_timestamp(void *ptr, uint32_t* timestamp)
    *timestamp = st.st_mtime;
 
    return true;
+}
+
+static inline bool
+disk_cache_get_function_identifier(void *ptr, uint32_t *id)
+{
+#ifdef HAVE_DL_ITERATE_PHDR
+   const struct build_id_note *note = NULL;
+   if ((note = build_id_find_nhdr_for_addr(ptr))) {
+      const uint8_t *id_sha1 = build_id_data(note);
+      assert(id_sha1);
+      *id = *id_sha1;
+      return true;
+   } else
+#endif
+   return disk_cache_get_function_timestamp(ptr, id);
 }
 #endif
 

anholt/mesa

commit 07a00a8729d709a4c43c828c64242c226607f09a

util: Change remaining uint32 cache ids to sha1
After discussion with Timothy Arceri. disk_cache_get_function_identifier
was using only the first byte of the sha1 build-id.  Replace
disk_cache_get_function_identifier with implementation from
radv_get_build_id.  Instead of writing a uint32_t it now writes to a
mesa_sha1.  All drivers using disk_cache_get_function_identifier are
updated accordingly.

Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Fixes: 83ea8dd99bb1 ("util: add disk_cache_get_function_identifier()")

src/amd/vulkan/radv_device.c
@@ -50,24 +50,6 @@
 #include "util/debug.h"
 #include "util/mesa-sha1.h"
 
-static bool
-radv_get_build_id(void *ptr, struct mesa_sha1 *ctx)
-{
-	uint32_t timestamp;
-
-#ifdef HAVE_DL_ITERATE_PHDR
-	const struct build_id_note *note = NULL;
-	if ((note = build_id_find_nhdr_for_addr(ptr))) {
-		_mesa_sha1_update(ctx, build_id_data(note), build_id_length(note));
-	} else
-#endif
-	if (disk_cache_get_function_timestamp(ptr, &timestamp)) {
-		_mesa_sha1_update(ctx, &timestamp, sizeof(timestamp));
-	} else
-		return false;
-	return true;
-}
-
 static int
 radv_device_get_cache_uuid(enum radeon_family family, void *uuid)
 {
@@ -78,8 +60,8 @@ radv_device_get_cache_uuid(enum radeon_family family, void *uuid)
 	memset(uuid, 0, VK_UUID_SIZE);
 	_mesa_sha1_init(&ctx);
 
-	if (!radv_get_build_id(radv_device_get_cache_uuid, &ctx) ||
-	    !radv_get_build_id(LLVMInitializeAMDGPUTargetInfo, &ctx))
+	if (!disk_cache_get_function_identifier(radv_device_get_cache_uuid, &ctx) ||
+	    !disk_cache_get_function_identifier(LLVMInitializeAMDGPUTargetInfo, &ctx))
 		return -1;
 
 	_mesa_sha1_update(&ctx, &family, sizeof(family));

src/gallium/drivers/nouveau/nouveau_screen.c
@@ -148,20 +148,21 @@ nouveau_screen_bo_get_handle(struct pipe_screen *pscreen,
 static void
 nouveau_disk_cache_create(struct nouveau_screen *screen)
 {
-   uint32_t mesa_id;
-   char *mesa_id_str;
-   int res;
+   struct mesa_sha1 ctx;
+   unsigned char sha1[20];
+   char cache_id[20 * 2 + 1];
 
-   if (disk_cache_get_function_identifier(nouveau_disk_cache_create,
-                                          &mesa_id)) {
-      res = asprintf(&mesa_id_str, "%u", mesa_id);
-      if (res != -1) {
-         screen->disk_shader_cache =
-            disk_cache_create(nouveau_screen_get_name(&screen->base),
-                              mesa_id_str, 0);
-         free(mesa_id_str);
-      }
-   }
+   _mesa_sha1_init(&ctx);
+   if (!disk_cache_get_function_identifier(nouveau_disk_cache_create,
+                                           &ctx))
+      return;
+
+   _mesa_sha1_final(&ctx, sha1);
+   disk_cache_format_hex_id(cache_id, sha1, 20 * 2);
+
+   screen->disk_shader_cache =
+      disk_cache_create(nouveau_screen_get_name(&screen->base),
+                        cache_id, 0);
 }
 
 int

src/gallium/drivers/r600/r600_pipe_common.c
@@ -854,27 +854,28 @@ static void r600_disk_cache_create(struct r600_common_screen *rscreen)
 	if (rscreen->debug_flags & DBG_ALL_SHADERS)
 		return;
 
-	uint32_t mesa_id;
-	if (disk_cache_get_function_identifier(r600_disk_cache_create,
-					       &mesa_id)) {
-		char *mesa_id_str;
-		int res = -1;
+	struct mesa_sha1 ctx;
+	unsigned char sha1[20];
+	char cache_id[20 * 2 + 1];
 
-		res = asprintf(&mesa_id_str, "%u", mesa_id);
-		if (res != -1) {
-			/* These flags affect shader compilation. */
-			uint64_t shader_debug_flags =
-				rscreen->debug_flags &
-				(DBG_FS_CORRECT_DERIVS_AFTER_KILL |
-				 DBG_UNSAFE_MATH);
+	_mesa_sha1_init(&ctx);
+	if (!disk_cache_get_function_identifier(r600_disk_cache_create,
+						&ctx))
+		return;
 
-			rscreen->disk_shader_cache =
-				disk_cache_create(r600_get_family_name(rscreen),
-						  mesa_id_str,
-						  shader_debug_flags);
-			free(mesa_id_str);
-		}
-	}
+	_mesa_sha1_final(&ctx, sha1);
+	disk_cache_format_hex_id(cache_id, sha1, 20 * 2);
+
+	/* These flags affect shader compilation. */
+	uint64_t shader_debug_flags =
+		rscreen->debug_flags &
+		(DBG_FS_CORRECT_DERIVS_AFTER_KILL |
+		 DBG_UNSAFE_MATH);
+
+	rscreen->disk_shader_cache =
+		disk_cache_create(r600_get_family_name(rscreen),
+				  cache_id,
+				  shader_debug_flags);
 }
 
 static struct disk_cache *r600_get_disk_shader_cache(struct pipe_screen *pscreen)

src/gallium/drivers/radeonsi/si_pipe.c
@@ -793,39 +793,39 @@ static void si_disk_cache_create(struct si_screen *sscreen)
 	if (sscreen->debug_flags & DBG_ALL_SHADERS)
 		return;
 
-	uint32_t mesa_id;
-	if (disk_cache_get_function_identifier(si_disk_cache_create, &mesa_id)) {
-		char *driver_id_str;
-		int res = -1;
-		uint32_t llvm_id;
-		if (disk_cache_get_function_identifier(LLVMInitializeAMDGPUTargetInfo,
-						       &llvm_id)) {
-			res = asprintf(&driver_id_str, "%u_%u", mesa_id, llvm_id);
-		}
+	struct mesa_sha1 ctx;
+	unsigned char sha1[20];
+	char cache_id[20 * 2 + 1];
 
-		if (res != -1) {
-			/* These flags affect shader compilation. */
-			#define ALL_FLAGS (DBG(FS_CORRECT_DERIVS_AFTER_KILL) | \
-					   DBG(SI_SCHED) | \
-					   DBG(GISEL) | \
-					   DBG(UNSAFE_MATH) | \
-					   DBG(NIR))
-			uint64_t shader_debug_flags = sscreen->debug_flags &
-						      ALL_FLAGS;
+	_mesa_sha1_init(&ctx);
 
-			/* Add the high bits of 32-bit addresses, which affects
-			 * how 32-bit addresses are expanded to 64 bits.
-			 */
-			STATIC_ASSERT(ALL_FLAGS <= UINT_MAX);
-			shader_debug_flags |= (uint64_t)sscreen->info.address32_hi << 32;
+	if (!disk_cache_get_function_identifier(si_disk_cache_create, &ctx) ||
+	    !disk_cache_get_function_identifier(LLVMInitializeAMDGPUTargetInfo,
+						&ctx))
+		return;
 
-			sscreen->disk_shader_cache =
-				disk_cache_create(sscreen->info.name,
-						  driver_id_str,
-						  shader_debug_flags);
-			free(driver_id_str);
-		}
-	}
+	_mesa_sha1_final(&ctx, sha1);
+	disk_cache_format_hex_id(cache_id, sha1, 20 * 2);
+
+	/* These flags affect shader compilation. */
+	#define ALL_FLAGS (DBG(FS_CORRECT_DERIVS_AFTER_KILL) |	\
+			   DBG(SI_SCHED) |			\
+			   DBG(GISEL) |				\
+			   DBG(UNSAFE_MATH) |			\
+			   DBG(NIR))
+	uint64_t shader_debug_flags = sscreen->debug_flags &
+		ALL_FLAGS;
+
+	/* Add the high bits of 32-bit addresses, which affects
+	 * how 32-bit addresses are expanded to 64 bits.
+	 */
+	STATIC_ASSERT(ALL_FLAGS <= UINT_MAX);
+	shader_debug_flags |= (uint64_t)sscreen->info.address32_hi << 32;
+
+	sscreen->disk_shader_cache =
+		disk_cache_create(sscreen->info.name,
+				  cache_id,
+				  shader_debug_flags);
 }
 
 struct pipe_screen *radeonsi_screen_create(struct radeon_winsys *ws,

src/util/disk_cache.h
@@ -33,6 +33,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <sys/stat.h>
+#include "util/mesa-sha1.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -115,18 +116,21 @@ disk_cache_get_function_timestamp(void *ptr, uint32_t* timestamp)
 }
 
 static inline bool
-disk_cache_get_function_identifier(void *ptr, uint32_t *id)
+disk_cache_get_function_identifier(void *ptr, struct mesa_sha1 *ctx)
 {
+   uint32_t timestamp;
+
 #ifdef HAVE_DL_ITERATE_PHDR
    const struct build_id_note *note = NULL;
    if ((note = build_id_find_nhdr_for_addr(ptr))) {
-      const uint8_t *id_sha1 = build_id_data(note);
-      assert(id_sha1);
-      *id = *id_sha1;
-      return true;
+      _mesa_sha1_update(ctx, build_id_data(note), build_id_length(note));
    } else
 #endif
-   return disk_cache_get_function_timestamp(ptr, id);
+   if (disk_cache_get_function_timestamp(ptr, &timestamp)) {
+      _mesa_sha1_update(ctx, &timestamp, sizeof(timestamp));
+   } else
+      return false;
+   return true;
 }
 #endif
 