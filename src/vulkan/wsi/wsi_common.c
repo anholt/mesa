anholt/mesa

commit baa38c144f6ab544bccabff3739631bab33e4cd7

vulkan/wsi: Use VK_EXT_pci_bus_info for DRM fd matching
This lets us avoid passing the DRM fd around all over the place and gets
us closer to layer utopia.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Eric Engestrom <eric.engestrom@intel.com>

src/vulkan/wsi/wsi_common.c
@@ -27,6 +27,7 @@
 #include "vk_util.h"
 
 #include <unistd.h>
+#include <xf86drm.h>
 
 VkResult
 wsi_device_init(struct wsi_device *wsi,
@@ -44,9 +45,18 @@ wsi_device_init(struct wsi_device *wsi,
 
 #define WSI_GET_CB(func) \
    PFN_vk##func func = (PFN_vk##func)proc_addr(pdevice, "vk" #func)
+   WSI_GET_CB(GetPhysicalDeviceProperties2);
    WSI_GET_CB(GetPhysicalDeviceMemoryProperties);
    WSI_GET_CB(GetPhysicalDeviceQueueFamilyProperties);
 #undef WSI_GET_CB
+
+   wsi->pci_bus_info.sType =
+      VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PCI_BUS_INFO_PROPERTIES_EXT;
+   VkPhysicalDeviceProperties2 pdp2 = {
+      .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2,
+      .pNext = &wsi->pci_bus_info,
+   };
+   GetPhysicalDeviceProperties2(pdevice, &pdp2);
 
    GetPhysicalDeviceMemoryProperties(pdevice, &wsi->memory_props);
    GetPhysicalDeviceQueueFamilyProperties(pdevice, &wsi->queue_family_count, NULL);
@@ -119,6 +129,32 @@ wsi_device_finish(struct wsi_device *wsi,
 #ifdef VK_USE_PLATFORM_XCB_KHR
    wsi_x11_finish_wsi(wsi, alloc);
 #endif
+}
+
+bool
+wsi_device_matches_drm_fd(const struct wsi_device *wsi, int drm_fd)
+{
+   drmDevicePtr fd_device;
+   int ret = drmGetDevice(drm_fd, &fd_device);
+   if (ret)
+      return false;
+
+   bool match = false;
+   switch (fd_device->bustype) {
+   case DRM_BUS_PCI:
+      match = wsi->pci_bus_info.pciDomain == fd_device->businfo.pci->domain &&
+              wsi->pci_bus_info.pciBus == fd_device->businfo.pci->bus &&
+              wsi->pci_bus_info.pciDevice == fd_device->businfo.pci->dev &&
+              wsi->pci_bus_info.pciFunction == fd_device->businfo.pci->func;
+      break;
+
+   default:
+      break;
+   }
+
+   drmFreeDevice(&fd_device);
+
+   return match;
 }
 
 VkResult
@@ -675,7 +711,6 @@ wsi_destroy_image(const struct wsi_swapchain *chain,
 
 VkResult
 wsi_common_get_surface_support(struct wsi_device *wsi_device,
-                               int local_fd,
                                uint32_t queueFamilyIndex,
                                VkSurfaceKHR _surface,
                                VkBool32* pSupported)
@@ -684,7 +719,7 @@ wsi_common_get_surface_support(struct wsi_device *wsi_device,
    struct wsi_interface *iface = wsi_device->wsi[surface->platform];
 
    return iface->get_support(surface, wsi_device,
-                             queueFamilyIndex, local_fd, pSupported);
+                             queueFamilyIndex, pSupported);
 }
 
 VkResult
@@ -805,7 +840,6 @@ wsi_common_get_surface_present_modes(struct wsi_device *wsi_device,
 
 VkResult
 wsi_common_get_present_rectangles(struct wsi_device *wsi_device,
-                                  int local_fd,
                                   VkSurfaceKHR _surface,
                                   uint32_t* pRectCount,
                                   VkRect2D* pRects)
@@ -813,14 +847,13 @@ wsi_common_get_present_rectangles(struct wsi_device *wsi_device,
    ICD_FROM_HANDLE(VkIcdSurfaceBase, surface, _surface);
    struct wsi_interface *iface = wsi_device->wsi[surface->platform];
 
-   return iface->get_present_rectangles(surface, wsi_device, local_fd,
+   return iface->get_present_rectangles(surface, wsi_device,
                                         pRectCount, pRects);
 }
 
 VkResult
 wsi_common_create_swapchain(struct wsi_device *wsi,
                             VkDevice device,
-                            int fd,
                             const VkSwapchainCreateInfoKHR *pCreateInfo,
                             const VkAllocationCallbacks *pAllocator,
                             VkSwapchainKHR *pSwapchain)
@@ -829,7 +862,7 @@ wsi_common_create_swapchain(struct wsi_device *wsi,
    struct wsi_interface *iface = wsi->wsi[surface->platform];
    struct wsi_swapchain *swapchain;
 
-   VkResult result = iface->create_swapchain(surface, device, wsi, fd,
+   VkResult result = iface->create_swapchain(surface, device, wsi,
                                              pCreateInfo, pAllocator,
                                              &swapchain);
    if (result != VK_SUCCESS)

src/amd/vulkan/radv_wsi.c
@@ -72,7 +72,6 @@ VkResult radv_GetPhysicalDeviceSurfaceSupportKHR(
 	RADV_FROM_HANDLE(radv_physical_device, device, physicalDevice);
 
 	return wsi_common_get_surface_support(&device->wsi_device,
-					      device->local_fd,
 					      queueFamilyIndex,
 					      surface,
 					      pSupported);
@@ -171,7 +170,6 @@ VkResult radv_CreateSwapchainKHR(
 
 	return wsi_common_create_swapchain(&device->physical_device->wsi_device,
 					   radv_device_to_handle(device),
-					   device->physical_device->local_fd,
 					   pCreateInfo,
 					   alloc,
 					   pSwapchain);
@@ -294,7 +292,6 @@ VkResult radv_GetPhysicalDevicePresentRectanglesKHR(
 	RADV_FROM_HANDLE(radv_physical_device, device, physicalDevice);
 
 	return wsi_common_get_present_rectangles(&device->wsi_device,
-						 device->local_fd,
 						 surface,
 						 pRectCount, pRects);
 }

src/amd/vulkan/radv_wsi_x11.c
@@ -45,7 +45,7 @@ VkBool32 radv_GetPhysicalDeviceXcbPresentationSupportKHR(
    return wsi_get_physical_device_xcb_presentation_support(
       &device->wsi_device,
       queueFamilyIndex,
-      device->local_fd, true,
+      true,
       connection, visual_id);
 }
 
@@ -60,7 +60,7 @@ VkBool32 radv_GetPhysicalDeviceXlibPresentationSupportKHR(
    return wsi_get_physical_device_xcb_presentation_support(
       &device->wsi_device,
       queueFamilyIndex,
-      device->local_fd, true,
+      true,
       XGetXCBConnection(dpy), visualID);
 }
 

src/intel/vulkan/anv_wsi.c
@@ -89,7 +89,6 @@ VkResult anv_GetPhysicalDeviceSurfaceSupportKHR(
    ANV_FROM_HANDLE(anv_physical_device, device, physicalDevice);
 
    return wsi_common_get_surface_support(&device->wsi_device,
-                                         device->local_fd,
                                          queueFamilyIndex,
                                          surface,
                                          pSupported);
@@ -183,7 +182,7 @@ VkResult anv_CreateSwapchainKHR(
    else
      alloc = &device->alloc;
 
-   return wsi_common_create_swapchain(wsi_device, _device, device->fd,
+   return wsi_common_create_swapchain(wsi_device, _device,
                                       pCreateInfo, alloc, pSwapchain);
 }
 
@@ -303,7 +302,6 @@ VkResult anv_GetPhysicalDevicePresentRectanglesKHR(
    ANV_FROM_HANDLE(anv_physical_device, device, physicalDevice);
 
    return wsi_common_get_present_rectangles(&device->wsi_device,
-                                            device->local_fd,
                                             surface,
                                             pRectCount, pRects);
 }

src/intel/vulkan/anv_wsi_x11.c
@@ -41,7 +41,7 @@ VkBool32 anv_GetPhysicalDeviceXcbPresentationSupportKHR(
    return wsi_get_physical_device_xcb_presentation_support(
       &device->wsi_device,
       queueFamilyIndex,
-      device->local_fd, false,
+      false,
       connection, visual_id);
 }
 
@@ -56,7 +56,7 @@ VkBool32 anv_GetPhysicalDeviceXlibPresentationSupportKHR(
    return wsi_get_physical_device_xcb_presentation_support(
       &device->wsi_device,
       queueFamilyIndex,
-      device->local_fd, false,
+      false,
       XGetXCBConnection(dpy), visualID);
 }
 

src/vulkan/wsi/wsi_common.h
@@ -97,6 +97,8 @@ struct wsi_device {
    VkPhysicalDeviceMemoryProperties memory_props;
    uint32_t queue_family_count;
 
+   VkPhysicalDevicePCIBusInfoPropertiesEXT pci_bus_info;
+
    bool supports_modifiers;
    uint64_t (*image_get_modifier)(VkImage image);
 
@@ -166,7 +168,6 @@ ICD_DEFINE_NONDISP_HANDLE_CASTS(VkIcdSurfaceBase, VkSurfaceKHR)
 
 VkResult
 wsi_common_get_surface_support(struct wsi_device *wsi_device,
-                               int local_fd,
                                uint32_t queueFamilyIndex,
                                VkSurfaceKHR surface,
                                VkBool32* pSupported);
@@ -201,7 +202,6 @@ wsi_common_get_surface_present_modes(struct wsi_device *wsi_device,
 
 VkResult
 wsi_common_get_present_rectangles(struct wsi_device *wsi,
-                                  int local_fd,
                                   VkSurfaceKHR surface,
                                   uint32_t* pRectCount,
                                   VkRect2D* pRects);
@@ -226,7 +226,6 @@ wsi_common_acquire_next_image2(const struct wsi_device *wsi,
 VkResult
 wsi_common_create_swapchain(struct wsi_device *wsi,
                             VkDevice device,
-                            int fd,
                             const VkSwapchainCreateInfoKHR *pCreateInfo,
                             const VkAllocationCallbacks *pAllocator,
                             VkSwapchainKHR *pSwapchain);

src/vulkan/wsi/wsi_common_display.c
@@ -803,7 +803,6 @@ static VkResult
 wsi_display_surface_get_support(VkIcdSurfaceBase *surface,
                                 struct wsi_device *wsi_device,
                                 uint32_t queueFamilyIndex,
-                                int local_fd,
                                 VkBool32* pSupported)
 {
    *pSupported = VK_TRUE;
@@ -934,27 +933,9 @@ wsi_display_surface_get_present_modes(VkIcdSurfaceBase *surface,
    return vk_outarray_status(&conn);
 }
 
-static bool
-fds_are_same_gpu(int fd1, int fd2)
-{
-   if (fd1 == -1 || fd2 == -1)
-      return false;
-
-   char *fd1_dev = drmGetRenderDeviceNameFromFd(fd1);
-   char *fd2_dev = drmGetRenderDeviceNameFromFd(fd2);
-
-   int ret = strcmp(fd1_dev, fd2_dev);
-
-   free(fd1_dev);
-   free(fd2_dev);
-
-   return ret == 0;
-}
-
 static VkResult
 wsi_display_surface_get_present_rectangles(VkIcdSurfaceBase *surface_base,
                                            struct wsi_device *wsi_device,
-                                           int local_fd,
                                            uint32_t* pRectCount,
                                            VkRect2D* pRects)
 {
@@ -962,7 +943,7 @@ wsi_display_surface_get_present_rectangles(VkIcdSurfaceBase *surface_base,
    wsi_display_mode *mode = wsi_display_mode_from_handle(surface->displayMode);
    VK_OUTARRAY_MAKE(out, pRects, pRectCount);
 
-   if (fds_are_same_gpu(local_fd, mode->connector->wsi->fd)) {
+   if (wsi_device_matches_drm_fd(wsi_device, mode->connector->wsi->fd)) {
       vk_outarray_append(&out, rect) {
          *rect = (VkRect2D) {
             .offset = { 0, 0 },
@@ -1730,7 +1711,6 @@ wsi_display_surface_create_swapchain(
    VkIcdSurfaceBase *icd_surface,
    VkDevice device,
    struct wsi_device *wsi_device,
-   int local_fd,
    const VkSwapchainCreateInfoKHR *create_info,
    const VkAllocationCallbacks *allocator,
    struct wsi_swapchain **swapchain_out)

src/vulkan/wsi/wsi_common_private.h
@@ -69,6 +69,9 @@ struct wsi_swapchain {
                              const VkPresentRegionKHR *damage);
 };
 
+bool
+wsi_device_matches_drm_fd(const struct wsi_device *wsi, int drm_fd);
+
 VkResult
 wsi_swapchain_init(const struct wsi_device *wsi,
                    struct wsi_swapchain *chain,
@@ -101,7 +104,6 @@ struct wsi_interface {
    VkResult (*get_support)(VkIcdSurfaceBase *surface,
                            struct wsi_device *wsi_device,
                            uint32_t queueFamilyIndex,
-                           int local_fd,
                            VkBool32* pSupported);
    VkResult (*get_capabilities2)(VkIcdSurfaceBase *surface,
                                  const void *info_next,
@@ -120,13 +122,11 @@ struct wsi_interface {
                                  VkPresentModeKHR* pPresentModes);
    VkResult (*get_present_rectangles)(VkIcdSurfaceBase *surface,
                                       struct wsi_device *wsi_device,
-                                      int local_fd,
                                       uint32_t* pRectCount,
                                       VkRect2D* pRects);
    VkResult (*create_swapchain)(VkIcdSurfaceBase *surface,
                                 VkDevice device,
                                 struct wsi_device *wsi_device,
-                                int local_fd,
                                 const VkSwapchainCreateInfoKHR* pCreateInfo,
                                 const VkAllocationCallbacks* pAllocator,
                                 struct wsi_swapchain **swapchain);

commit c210d0c3b72ac426ee18b43f036e1a852a39950b

vulkan/wsi: use the drmGetDevice2() API
On older kernels, the drmGetDevice() call will wake up all the GPUs
on the system, while fetching the PCI revision.

Use the 2 version of the API and pass flags == 0, so we don't fetch the
device PCI revision, since we don't need that information.

Fixes: baa38c144f6 ("vulkan/wsi: Use VK_EXT_pci_bus_info for DRM fd matching")
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/vulkan/wsi/wsi_common.c
@@ -135,7 +135,7 @@ bool
 wsi_device_matches_drm_fd(const struct wsi_device *wsi, int drm_fd)
 {
    drmDevicePtr fd_device;
-   int ret = drmGetDevice(drm_fd, &fd_device);
+   int ret = drmGetDevice2(drm_fd, 0, &fd_device);
    if (ret)
       return false;
 