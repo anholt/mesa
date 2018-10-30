anholt/mesa

commit bb84fa146f2252f22999205a2904d8a948bffd3b

util: use C99 declaration in the for-loop hash_table_foreach() macro
Signed-off-by: Eric Engestrom <eric@engestrom.ch>
Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

src/gallium/drivers/vc4/vc4_program.c
@@ -2975,7 +2975,6 @@ vc4_shader_state_delete(struct pipe_context *pctx, void *hwcso)
         struct vc4_context *vc4 = vc4_context(pctx);
         struct vc4_uncompiled_shader *so = hwcso;
 
-        struct hash_entry *entry;
         hash_table_foreach(vc4->fs_cache, entry) {
                 delete_from_cache_if_matches(vc4->fs_cache, &vc4->prog.fs,
                                              entry, so);

src/amd/common/ac_nir_to_llvm.c
@@ -3635,7 +3635,6 @@ static void visit_post_phi(struct ac_nir_context *ctx,
 
 static void phi_post_pass(struct ac_nir_context *ctx)
 {
-	struct hash_entry *entry;
 	hash_table_foreach(ctx->phis, entry) {
 		visit_post_phi(ctx, (nir_phi_instr*)entry->key,
 		               (LLVMValueRef)entry->data);

src/broadcom/compiler/vir_live_variables.c
@@ -180,8 +180,6 @@ vir_setup_def(struct v3d_compile *c, struct qblock *block, int ip,
 static void
 sf_state_clear(struct hash_table *partial_update_ht)
 {
-        struct hash_entry *entry;
-
         hash_table_foreach(partial_update_ht, entry) {
                 struct partial_update_state *state = entry->data;
 

src/broadcom/compiler/vir_lower_uniforms.c
@@ -134,7 +134,6 @@ vir_lower_uniforms(struct v3d_compile *c)
                  */
                 uint32_t max_count = 0;
                 uint32_t max_index = 0;
-                struct hash_entry *entry;
                 hash_table_foreach(ht, entry) {
                         uint32_t count = (uintptr_t)entry->data;
                         uint32_t index = (uintptr_t)entry->key - 1;

src/compiler/glsl/ast_to_hir.cpp
@@ -6645,7 +6645,6 @@ ast_case_statement_list::hir(exec_list *instructions,
     * if default should be chosen or not.
     */
    if (!default_case.is_empty()) {
-      struct hash_entry *entry;
       ir_factory body(instructions, state);
 
       ir_expression *cmp = NULL;

src/compiler/glsl/link_uniform_blocks.cpp
@@ -362,7 +362,6 @@ create_buffer_blocks(void *mem_ctx, struct gl_context *ctx,
                       ctx->Const.UseSTD430AsDefaultPacking);
 
    unsigned i = 0;
-   struct hash_entry *entry;
    hash_table_foreach (block_hash, entry) {
       const struct link_uniform_block_active *const b =
          (const struct link_uniform_block_active *) entry->data;
@@ -429,7 +428,6 @@ link_uniform_blocks(void *mem_ctx,
    unsigned num_ubo_variables = 0;
    unsigned num_ssbo_variables = 0;
    count_block_size block_size;
-   struct hash_entry *entry;
 
    hash_table_foreach (block_hash, entry) {
       struct link_uniform_block_active *const b =

src/compiler/glsl/opt_constant_propagation.cpp
@@ -380,7 +380,6 @@ ir_constant_propagation_visitor::visit_enter(ir_if *ir)
       acp->make_empty();
       killed_all = true;
    } else {
-      hash_entry *htk;
       hash_table_foreach(new_kills, htk)
          kill((ir_variable *) htk->key, (uintptr_t) htk->data);
    }
@@ -420,7 +419,6 @@ ir_constant_propagation_visitor::handle_loop(ir_loop *ir, bool keep_acp)
    this->acp = orig_acp;
    this->killed_all = this->killed_all || orig_killed_all;
 
-   hash_entry *htk;
    hash_table_foreach(new_kills, htk) {
       kill((ir_variable *) htk->key, (uintptr_t) htk->data);
    }

src/compiler/glsl/opt_constant_variable.cpp
@@ -190,7 +190,6 @@ do_constant_variable(exec_list *instructions)
                                   _mesa_key_pointer_equal);
    v.run(instructions);
 
-   struct hash_entry *hte;
    hash_table_foreach(v.ht, hte) {
       struct assignment_entry *entry = (struct assignment_entry *) hte->data;
 

commit 68657d76b9ac5c302a0f492174137028e77bd35b

vc4: Fix unused variable warning.
Fixes: bb84fa146f22 ("util: use C99 declaration in the for-loop hash_table_foreach() macro")

src/gallium/drivers/vc4/vc4_program.c
@@ -3031,7 +3031,6 @@ vc4_program_fini(struct pipe_context *pctx)
 {
         struct vc4_context *vc4 = vc4_context(pctx);
 
-        struct hash_entry *entry;
         hash_table_foreach(vc4->fs_cache, entry) {
                 struct vc4_compiled_shader *shader = entry->data;
                 vc4_bo_unreference(&shader->bo);