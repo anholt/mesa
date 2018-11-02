6328536ff28ca26f2ad4e6f2c956c467acebda88
Eric Anholt <eric@anholt.net>
2018-11-02 14:35:06 -0700

    gbm: Introduce a helper function for printing GBM format names.
    This requires that the caller make a little (stack) allocation to store
    the string.
    
    v2: Use gbm_format_canonicalize (suggested by Daniel)
    
    Reviewed-by: Eric Engestrom <eric.engestrom@intel.com>
    Reviewed-by: Daniel Stone <daniels@collabora.com>

--- src/gbm/main/gbm.c
+++ src/gbm/main/gbm.c
@@ -711,3 +711,23 @@ gbm_format_canonicalize(uint32_t gbm_format)
       return gbm_format;
    }
 }
+
+/**
+ * Returns a string representing the fourcc format name.
+ *
+ * \param desc Caller-provided storage for the format name string.
+ * \return String containing the fourcc of the format.
+ */
+GBM_EXPORT char *
+gbm_format_get_name(uint32_t gbm_format, struct gbm_format_name_desc *desc)
+{
+   gbm_format = gbm_format_canonicalize(gbm_format);
+
+   desc->name[0] = gbm_format;
+   desc->name[1] = gbm_format >> 8;
+   desc->name[2] = gbm_format >> 16;
+   desc->name[3] = gbm_format >> 24;
+   desc->name[4] = 0;
+
+   return desc->name;
+}
