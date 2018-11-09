ac324a6809c09c54d3b0bfdb00e5e62987ec4ad8
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2018-11-09 16:49:12 +0000

    intel/aub_viewer: fix dynamic state printing
    Identical fix to :
    
    commit cbd4bc1346f7397242e157bb66099b950a8c5643
    Author: Jason Ekstrand <jason.ekstrand@intel.com>
    Date:   Fri Aug 24 16:04:03 2018 -0500
    
        intel/batch_decoder: Fix dynamic state printing
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Toni Lönnberg <toni.lonnberg@intel.com>

--- src/intel/tools/aubinator_viewer_decoder.cpp
+++ src/intel/tools/aubinator_viewer_decoder.cpp
@@ -651,10 +651,10 @@ decode_dynamic_state_pointers(struct aub_viewer_decode_ctx *ctx,
 
    for (int i = 0; i < count; i++) {
       ImGui::Text("%s %d", struct_type, i);
-      aub_viewer_print_group(ctx, state, state_offset, state_map);
+      aub_viewer_print_group(ctx, state, state_addr, state_map);
 
       state_addr += state->dw_length * 4;
-      state_map += state->dw_length;
+      state_map += state->dw_length * 4;
    }
 }
 
