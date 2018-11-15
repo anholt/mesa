anholt/mesa

commit c9733649670243a1a6eb7ca2c376bd27960f8d8a

egl: add missing glvnd entrypoint for EGL_ANDROID_blob_cache
Fixes dEQP-EGL.functional.get_proc_address.extension.egl_android_blob_cache
on builds with glvnd enabled.

Fixes: 6f5b57093b3462a54e9c7 "egl: add support for EGL_ANDROID_blob_cache"
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

src/egl/generate/eglFunctionList.py
@@ -196,6 +196,9 @@ EGL_FUNCTIONS = (
     # EGL_ANDROID_native_fence_sync
     _eglFunc("eglDupNativeFenceFDANDROID",           "display"),
 
+    # EGL_ANDROID_blob_cache
+    ("eglSetBlobCacheFuncsANDROID",          "display"),
+
     # EGL_EXT_image_dma_buf_import_modifiers
     _eglFunc("eglQueryDmaBufFormatsEXT",             "display"),
     _eglFunc("eglQueryDmaBufModifiersEXT",           "display"),

commit 2b2f790e594cde3aa0d4dcebafc9a072651cd62a

egl: fix bad rebase
I screwed up a rebase over a refactor and didn't notice locally because
the uncommitted refactor hid the issue.

Fixes: c9733649670243a1a6eb "egl: add missing glvnd entrypoint for EGL_ANDROID_blob_cache"
Signed-off-by: Eric Engestrom <eric.engestrom@intel.com>

src/egl/generate/eglFunctionList.py
@@ -197,7 +197,7 @@ EGL_FUNCTIONS = (
     _eglFunc("eglDupNativeFenceFDANDROID",           "display"),
 
     # EGL_ANDROID_blob_cache
-    ("eglSetBlobCacheFuncsANDROID",          "display"),
+    _eglFunc("eglSetBlobCacheFuncsANDROID",          "display"),
 
     # EGL_EXT_image_dma_buf_import_modifiers
     _eglFunc("eglQueryDmaBufFormatsEXT",             "display"),